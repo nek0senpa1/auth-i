4fc5d1d99440b8a5440aa313d699aecfc28de264
amberm <nekosenpai@gmail.com>
2019-03-04 14:47:44 -0700

    got some things thinging :|_

--- index.js
+++ index.js
@@ -18,21 +18,39 @@ const Softy = require('./userstuff/softserver');
 
 
 
-server.post('api/newuser', (rec,rez) => {
+server.post('/api/newuser', (rec,rez) => {
     var newUser = rec.body;
 
     const hasher = bcrypt.hashSync(newUser.password, 8);
 
     newUser.password = hasher;
 
-    Softy.putStuff(newUser)
+    Softy.add(newUser)
     .then(nooby => {
         rez.status(201).json({message: `New User ${nooby} is now registered`})
     })
     .catch(err => {
-        rez.status(500).json(err);
+        rez.status(500).json({message: 'user name already exists'})
     })
 });
+
+
+server.post('/api/login', (rec,rez) => {
+    var {username, password} = rec.body;
+
+    Softy.getBy({name: username}).first()
+
+    // note... all that doubling/sending stuff confuses me... thus I re-write things
+    // and change the names, so I know exactly what it's doing :) 
+
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            rez.status(201).json({message: `Welcome ${user.name} ?|?|?`});
+        } else {
+            rez.status(402).json({message: "Invalid Login Attempt"})
+        }
+    }).catch(err => { rez.status(500).json({mess: "You See This?  You F'd Up Somewhere..."})})
+})
 
 
 
--- userstuff/softserver.js
+++ userstuff/softserver.js
@@ -4,6 +4,7 @@ const knexConfig = require('../knexfile');
 const deebee = knex(knexConfig.development);
 
 module.exports = {
+    add,
     putStuff,
     getStuff,
     getBy,
@@ -23,8 +24,20 @@ function getById(stuff) {
 }
 
 function putStuff(item) {
-    deebee('users').insert(item);
-    setTimeout( function(){
-        return item;
-      }, 2500);
+    deebee('users').insert(item)
+    .then(yay => {
+        setTimeout( function() {
+        return (item);
+        }, 2500);
+    })
+    .catch(err => {
+        return err;
+    })
 }
+
+
+async function add(user) {
+    const [id] = await deebee('users').insert(user);
+  
+    return (id);
+  }
