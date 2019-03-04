54a2f64a825b995204880eb4097284002cb59e1f
amberm <nekosenpai@gmail.com>
2019-03-04 12:32:46 -0700

    dependencies and sanity check, done

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (rec,rez) =>{
+    rez.send('L00k5 L1K3 W3 M4D3 1T')
+})
+
+const port = process.env.PORT || 5500;
+server.listen(port, () => console.log(`Heyo, we is running on port ${port} !!! `));
