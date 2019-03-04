06dcb666e1c2886c6789c1c6fd58851595677307
amberm <nekosenpai@gmail.com>
2019-03-04 12:43:38 -0700

    getting the players in place

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+const doublebee = knex(knexConfig.development);
+
+server.use(knexConfig)
+
 server.get('/', (rec,rez) =>{
     rez.send('L00k5 L1K3 W3 M4D3 1T')
 })
